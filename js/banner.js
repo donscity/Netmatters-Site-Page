0a772ddb4e2f61af2622383ae1a14ea19628f6cb
donscity <donscity@gmail.com>
2021-06-22 15:09:02 +0100

    added slick plugin (banner scrolling)

---
+++ js/banner.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slick').slick({
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        pauseOnFocus: false,
+        mobileFirst: true,
+    });
+});
--- js/sticky-header.js
+++ js/sticky-header.js
@@ -8,13 +8,15 @@ var sticky = header.offsetTop;
 
 let lastScroll = 0;
 
-let headHeight = header.offsetHeight;
+
 
 function scrollMenu(){ 
-    var currentScroll = page.scrollTop;
+    let headHeight = header.offsetHeight;
+    let currentScroll = page.scrollTop;
     if (currentScroll < lastScroll && currentScroll > headHeight){
         header.classList.remove("slide-up");
         header.classList.add("sticky");
+        header.style.width = "calc( 100% - "+getScrollbarWidth()+"px )";
         pageTop.style.paddingTop = headHeight+"px";
     } else if (currentScroll > lastScroll){
         if(currentScroll >= headHeight){
@@ -30,5 +32,30 @@ function scrollMenu(){
 function reset(){
     header.classList.remove("sticky");
     pageTop.style.paddingTop = "0";
+    header.style.width = "100%";
 }
 
+
+
+function getScrollbarWidth() {
+
+    // Creating invisible container
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+    document.body.appendChild(outer);
+  
+    // Creating inner element and placing it in the container
+    const inner = document.createElement('div');
+    outer.appendChild(inner);
+  
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+  
+    // Removing temporary elements from the DOM
+    outer.parentNode.removeChild(outer);
+  
+    return scrollbarWidth;
+  
+  }
