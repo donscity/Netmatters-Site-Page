54ab62a9f1f597896c95963e8badf67995c772c2
donscity <donscity@gmail.com>
2021-06-24 16:55:08 +0100

    Update cookie-consent.js

--- js/cookie-consent.js
+++ js/cookie-consent.js
@@ -1,7 +1,7 @@
 let $cookie = $('#cookie-popup')
 let $acceptBtn = $('#cookie-popup .btn.accept')
 
-if (ReadCookie('cookieConsent') == 'false') {$cookie.css('display', 'block')}
+if (ReadCookie('cookieConsent') == false) {$cookie.css('display', 'block')}
 
 $acceptBtn.click( () => {
     SetCookie('cookieConsent', 'true', 100); 
