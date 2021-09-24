f714ea5a58f3e78ea28da033cbd9fd5dc19ddff3
donscity <donscity@gmail.com>
2021-09-24 22:02:01 +0100

    added contact form functionality

---
+++ contact.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Netmatters | Full Service Digital Agency | Norwich, Norfolk | Netmatters</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" href="images/logos/favicon.ico">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link href="fonts/awsome-fonts/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="js/slick-1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="js/slick-1.8.1/slick/slick-theme.css"/>
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<!-- ------------------------------------------------------------------------------------------------------- -->
+<body>
+    <?php include "reusable/cookie.html" ?>
+    <?php include "reusable/side_bar.html" ?>
+    <div id="page">
+        <div id=top>
+            <?php include "reusable/header.html" ?> 
+<!-- ------------------------------------------------------------------------------------------------------- -->
+            <section class="page-span">
+                <div class="shadow">
+                    <div class="page-center news-head">
+                        <b><a href="index.php">Home </a></b> / Our Offices
+                    </div>
+                </div>
+                <div class="page-center content-section">
+                    <h1>Our Offices</h1>
+                    <div class="card-container article">
+                        <div class="article card web">
+                            <a class="img" href="https://www.netmatters.co.uk/cambridge-office">
+                                <img src="images/location/cambridge.jpg" alt="Cambridge Office"></a>
+                            <div class="content">
+                                <a href="https://www.netmatters.co.uk/cambridge-office"><h3>Cambridge Office</h4></a>
+                                <p>
+                                    Unit 1.28, <br>
+                                    St John's Innovation Centre, <br>
+                                    Cowley Road, Milton, <br>
+                                    Cambridge, <br>
+                                    CB4 0WS
+                                </p>
+                                <a href="https://www.netmatters.co.uk/01223375772"><h4 class="link">01223 37 57 72</h4></a>
+                                <a class="btn" href="https://www.netmatters.co.uk/cambridge-office">View More</a>
+                            </div>
+                            
+                        </div>
+                        <div class="article card web">
+                            <a class="img" href="https://www.netmatters.co.uk/wymondham-office">
+                                <img src="images/location/wymondham.jpg" alt="Wymondham Office"></a>
+                            <div class="content">
+                                <a href="https://www.netmatters.co.uk/wymondham-office"><h3>Wymondham Office</h4></a>
+                                <p>
+                                    Unit 15,  <br>
+                                    Penfold Drive, <br>
+                                    Gateway 11 Business Park, <br>
+                                    Wymondham, Norfolk, <br>
+                                    NR18 0WZ <br>
+                                </p>
+                                <a href="https://www.netmatters.co.uk/01603704020"><h4 class="link">01603 70 40 20</h4></a>
+                                <a class="btn" href="https://www.netmatters.co.uk/wymondham-office">View More</a>
+                            </div>
+                            
+                        </div>
+                        <div class="article card web">
+                            <a class="img" href="https://www.netmatters.co.uk/yarmouth-office">
+                                <img src="images/location/yarmouth.jpg" alt="Great Yarmouth Office"></a>
+                            <div class="content">
+                                <a href="https://www.netmatters.co.uk/yarmouth-office"><h3>Great Yarmouth Office</h4></a>
+                                <p>
+                                    Suite F23, <br>
+                                    Beacon Innovation Centre, <br>
+                                    Beacon Park, Gorleston, <br>
+                                    Great Yarmouth, Norfolk, <br>
+                                    NR31 7RA <br>
+                                </p>
+                                <a href="https://www.netmatters.co.uk/01493603204"><h4 class="link">01493 60 32 04</h4></a>
+                                <a class="btn" href="https://www.netmatters.co.uk/yarmouth-office">View More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex-reverse">
+                        <div>
+                            <b>
+                            Email us on:
+                            <a>sales@netmatters.com</a>
+                            <br>
+                            Business hours:
+                            <br>
+                            Monday - Friday 07:00 - 18:00 
+                            <br>
+                            <a>Out of Hours IT Support  <i class="fas fa-chevron-down"></i> </a>
+                            </b>
+                            <br>
+                            Netmatters IT are offering an Out of Hours service for Emergency and Critical tasks.
+                            <br>
+                            <b>
+                            Monday - Friday 18:00 - 22:00 Saturday 08:00 - 16:00
+                            Sunday 10:00 - 18:00
+                            </b>
+                            <br>
+                            To log a critical task, you will need to call our main line number and select Option 2 to leave an Out of Hours  voicemail. A technician will contact you on the number provided within 45 minutes of your call. 
+                        </div>
+                        <form class="form-box" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" id="sign-up" action="#" accept-charset="UTF-8">
+                            <?php include "php/contact-check.php" ?>
+                            
+                            <?php if ($submitErr == true) { ?>
+                            <div class='submitnt'>Failed to Summit</div>
+                            <?php } ?>
+                            <?php if ($sent == true) { ?>
+                            <div class='submitted'>Submitted Successfully</div>
+                            <?php } ?>
+
+                            <div class="input-group">
+                                <div>
+                                    <label class="required" for="name"><b>Your Name</b></label>
+                                    <input class="btn text" type="text" id="name" name="name" required value="<?php echo $name;?>">
+                                </div>
+                                <div>
+                                    <label class="required" for="email"><b>Your Email</b></label>
+                                    <input class="btn text" type="email" id="email" name="email" required value="<?php echo $email;?>">
+                                </div>
+                                <div>
+                                    <label class="required" for="phone"><b>Your Telephone Number</b></label>
+                                    <input class="btn text" type="tel" id="phone" name="phone" required value="<?php echo $phone;?>">
+                                </div>
+                                <div>
+                                    <label class="required" for="subject"><b>Subject</b></label>
+                                    <input class="btn text" type="text" id="subject" name="subject" required value="<?php echo $subject;?>">
+                                </div>
+                                <div>
+                                    <label class="required" for="message"><b>Message</b></label>
+                                    <textarea class="btn text" id="message" name="message" required> <?php echo $message;?> </textarea>
+                                </div>
+                            
+                                <label class="checkbox">
+                                    <span>
+                                        <input type="checkbox" id="marketinginfo" name="marketinginfo" value="true" required>
+                                        <span class="fake-checkbox"><i class="fas fa-check"></i></span>
+                                    </span>
+                                    <b>Please tick this box if you wish to receive marketing information from us. Please see our <a href="https://www.netmatters.co.uk/privacy-policy">Privacy Policy</a> for more information on how we use your data.</b>
+                                </label>
+                                <input class="btn alt alt2" type="submit" value="Send Enquiry">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+<!-- ------------------------------------------------------------------------------------------------------------------------ -->
+            <?php include "reusable/footer.html" ?>
+        </div>
+    </div>
+<!-- ------------------------------------------------------------------------------------------------------------- -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" 
+            integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" 
+            crossorigin="anonymous">
+    </script>
+    <script src="js/cookie-control.js"></script>
+    <script src="js/cookie-consent.js"></script>
+    <script src="js/sticky-header.js"></script>
+    <script src="js/side-menu.js"></script>
+</body>
+</html>
